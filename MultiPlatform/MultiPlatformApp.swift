4f90ede9ef3e6c5d857a554fc107827dfdaac27f
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-07-31 16:11:35 +0900

    Initial Commit

---
+++ MultiPlatform/MultiPlatformApp.swift
@@ -0,0 +1,15 @@
+//
+// MultiPlatformApp.swift
+//
+
+
+import SwiftUI
+
+@main
+struct MultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
